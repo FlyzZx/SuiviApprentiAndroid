3be01ab8a23f6b6c04655b577b66e150e6582e57
FlyzZx <nicolasfaraci@outlook.com>
2016-05-27 20:09:11 +0200

    Annotations

--- app/src/main/java/projet/suiviapprenti/SuiviApprenti.java
+++ app/src/main/java/projet/suiviapprenti/SuiviApprenti.java
@@ -9,9 +9,14 @@ import java.net.CookieManager;
  * Created by nicol on 27/05/2016.
  */
 public class SuiviApprenti extends Application {
+
+    /**
+     * Lancé au début de l'appli, cette methode permet d'executer ce qui doit l'etre une seule et unique fois
+     */
     @Override
     public void onCreate() {
         super.onCreate();
+        //Persistance des cookies
         CookieManager cookieManager = new CookieManager();
         CookieHandler.setDefault(cookieManager);
     }
