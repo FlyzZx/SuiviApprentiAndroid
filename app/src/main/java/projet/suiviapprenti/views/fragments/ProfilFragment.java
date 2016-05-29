d58c433bdde92b3a98d7e0fdc7e84b5cca26f390
FlyzZx <nicolasfaraci@outlook.com>
2016-05-29 18:58:49 +0200

    Finalize views infos pers
    -Finalize profil view
    -Add button moodif profil

--- app/src/main/java/projet/suiviapprenti/views/fragments/ProfilFragment.java
+++ app/src/main/java/projet/suiviapprenti/views/fragments/ProfilFragment.java
@@ -4,9 +4,11 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import projet.suiviapprenti.R;
@@ -18,14 +20,17 @@ import projet.suiviapprenti.views.activitys.LoginActivity;
 /**
  * Created by nicol on 29/05/2016.
  */
-public class ProfilFragment extends Fragment {
+public class ProfilFragment extends Fragment implements View.OnClickListener {
     ProfilForm profilForm;
-    TextView nom, prenom, classe, naissance, adresse, mail, tel, site, mission;
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.infos_profil_fragment, container, false);
+    TextView nom, prenom, classe, naissance, adresse, mail, tel, site, mission, nomEntreprise, brancheEntreprise, nbSalariesEntreprise;
+    Button btnModif;
+
+    private void initialize(View v) {
         profilForm = new ProfilForm();
+
+        btnModif = (Button) v.findViewById(R.id.button_infosProfil_modifier);
+        btnModif.setOnClickListener(this);
+
         nom = (TextView) v.findViewById(R.id.textView_infosProfil_nom);
         prenom = (TextView) v.findViewById(R.id.textView_infosProfil_prenom);
         classe = (TextView) v.findViewById(R.id.textView_infosProfil_classe);
@@ -35,23 +40,56 @@ public class ProfilFragment extends Fragment {
         tel = (TextView) v.findViewById(R.id.textView_infosProfil_tel);
         site = (TextView) v.findViewById(R.id.textView_infosProfil_site);
         mission = (TextView) v.findViewById(R.id.textView_infosProfil_mission);
+        nomEntreprise = (TextView) v.findViewById(R.id.textView_infosProfil_nomEntreprise);
+        brancheEntreprise = (TextView) v.findViewById(R.id.textView_infosProfil_bancheEntreprise);
+        nbSalariesEntreprise = (TextView) v.findViewById(R.id.textView_infosProfil_nbSalariesEntreprise);
+    }
+
+    private void updateTextviews(Apprenti app) {
+        //MAJ TEXTVIEWS
+        nom.setText(app.getNom());
+        prenom.setText(app.getPrenom());
+        classe.setText(app.getClasse().getNom() + " - " + app.getClasse().getAnnee());
+        naissance.setText(app.getDateNaissance().toString());
+        adresse.setText(app.getCoordonnees().getRue() + " " + app.getCoordonnees().getCodePostal() + " " + app.getCoordonnees().getVille());
+        mail.setText(app.getCoordonnees().getEmail());
+        tel.setText(app.getCoordonnees().getTelephone() + " / " + app.getCoordonnees().getMobile());
+        site.setText(app.getCoordonnees().getSite());
+        mission.setText(app.getMissionPrincipale());
+
+        nomEntreprise.setText(app.getEntreprise().getNomEntreprise());
+        brancheEntreprise.setText(app.getEntreprise().getBranche());
+        nbSalariesEntreprise.setText(app.getEntreprise().getNbSalaries());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.infos_profil_fragment, container, false);
+        initialize(v);
         try {
             Apprenti app = profilForm.getInfosPersonnelles();
-            //MAJ TEXTVIEWS
-            nom.setText(app.getNom());
-            prenom.setText(app.getPrenom());
-            classe.setText(app.getClasse().getNom() + " - " + app.getClasse().getAnnee());
-            naissance.setText(app.getDateNaissance().toString());
-            adresse.setText(app.getCoordonnees().getRue() + " " + app.getCoordonnees().getCodePostal() + " " + app.getCoordonnees().getVille());
-            mail.setText(app.getCoordonnees().getEmail());
-            tel.setText(app.getCoordonnees().getTelephone() + " / " + app.getCoordonnees().getMobile());
-            site.setText(app.getCoordonnees().getSite());
-            mission.setText(app.getMissionPrincipale());
+            updateTextviews(app);
+
         } catch (Exception e) {
             Intent intent = new Intent(v.getContext(), LoginActivity.class);
             startActivity(intent);
         }
 
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.button_infosProfil_modifier:
+                Snackbar.make(v, "CALLBACK MODIFIER PROFIL", Snackbar.LENGTH_LONG).show();
+                break;
+
+            default:
+                break;
+        }
     }
 }
