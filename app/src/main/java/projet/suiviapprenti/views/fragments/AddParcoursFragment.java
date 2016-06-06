9eb1413c08eadfae0bdf24cc06029e117923ac7a
FlyzZx <nicolasfaraci@outlook.com>
2016-06-06 19:47:15 +0200

    Add layout for add/updt parcours and more
    -Add soft input mode to pop up the fields
    -Add parcours fragment and layout

--- app/src/main/java/projet/suiviapprenti/views/fragments/AddParcoursFragment.java
+++ app/src/main/java/projet/suiviapprenti/views/fragments/AddParcoursFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import projet.suiviapprenti.R;
 
@@ -13,10 +16,22 @@ import projet.suiviapprenti.R;
  * Created by nicol on 06/06/2016.
  */
 public class AddParcoursFragment extends Fragment {
+    Spinner fonctions;
+    ArrayAdapter spAdapt;
+    String[] listFonctions = {"Développeur", "Ingénieur", "Webmaster"};
+
+    private void initialize(View v) {
+        fonctions = (Spinner) v.findViewById(R.id.spinner_parcours_fonction);
+        spAdapt = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_spinner_dropdown_item, listFonctions);
+        fonctions.setAdapter(spAdapt);
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.modifier_parcours_fragment, container, false);
+        initialize(v);
 
         return v;
     }
